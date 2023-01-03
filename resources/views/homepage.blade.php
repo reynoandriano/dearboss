d9263a5da04f4639f182cb32ec68fafbb5c6b4ff
Reyno Andriano <reyno@gmail.com>
2023-01-03 07:31:11 +0700

    Ready for initial deploy

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,7 +7,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', '8BOSS') }}</title>
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <meta name="description" content="Your daily dose of funny memes, GIFs, videos and weird news stories. We deliver hundreds of new memes daily and much more humor anywhere you go.">
+    <link rel="canonical" href="http://127.0.0.1:8000/">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@9gag">
+    <meta name="twitter:image" content="https://images-cdn.9gag.com/img/9gag-og.png">
+    <meta property="og:title" content="9GAG - Best Funny Memes and Breaking News">
+    <meta property="og:site_name" content="9GAG">
+    <meta property="og:description"  content="Your daily dose of funny memes, GIFs, videos and weird news stories. We deliver hundreds of new memes daily and much more humor anywhere you go.">
+    <meta property="og:url" content="http://9gag.com/">
+    <meta property="og:image" content="https://images-cdn.9gag.com/img/9gag-og.png">
+    <meta property="og:type" content="website">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://9gag.com/s/fab0aa49/20b08b48ab90040fe60737140aa062159e973a80/static/dist/es8/mobile/img/apple-touch-icon-180.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="https://9gag.com/s/fab0aa49/20b08b48ab90040fe60737140aa062159e973a80/static/dist/es8/mobile/img/apple-touch-icon-152.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="https://9gag.com/s/fab0aa49/20b08b48ab90040fe60737140aa062159e973a80/static/dist/es8/mobile/img/apple-touch-icon-120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="https://9gag.com/s/fab0aa49/20b08b48ab90040fe60737140aa062159e973a80/static/dist/es8/mobile/img/apple-touch-icon-76.png">
+    <meta name="theme-color" content="#000">
+    <link rel="stylesheet" href="/fonts/inter.css">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
@@ -18,11 +34,12 @@
             <a href="/">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-9 h-9 text-red-500 drop-shadow-md">
                     <path d="M21.721 12.752a9.711 9.711 0 00-.945-5.003 12.754 12.754 0 01-4.339 2.708 18.991 18.991 0 01-.214 4.772 17.165 17.165 0 005.498-2.477zM14.634 15.55a17.324 17.324 0 00.332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 00.332 4.647 17.385 17.385 0 005.268 0zM9.772 17.119a18.963 18.963 0 004.456 0A17.182 17.182 0 0112 21.724a17.18 17.18 0 01-2.228-4.605zM7.777 15.23a18.87 18.87 0 01-.214-4.774 12.753 12.753 0 01-4.34-2.708 9.711 9.711 0 00-.944 5.004 17.165 17.165 0 005.498 2.477zM21.356 14.752a9.765 9.765 0 01-7.478 6.817 18.64 18.64 0 001.988-4.718 18.627 18.627 0 005.49-2.098zM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 001.988 4.718 9.765 9.765 0 01-7.478-6.816zM13.878 2.43a9.755 9.755 0 016.116 3.986 11.267 11.267 0 01-3.746 2.504 18.63 18.63 0 00-2.37-6.49zM12 2.276a17.152 17.152 0 012.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0112 2.276zM10.122 2.43a18.629 18.629 0 00-2.37 6.49 11.266 11.266 0 01-3.746-2.504 9.754 9.754 0 016.116-3.985z" />
-                </svg>                  
+                </svg>
+                <span class="sr-only">8BOSS</span>
             </a>
         </div>
 
-        <div class="flex flex-1 justify-center px-1 lg:ml-6 lg:justify-end">
+        <div class="flex flex-1 justify-center px-2">
             <div class="w-full max-w-lg lg:max-w-xs">
                 <label for="search" class="sr-only">Search</label>
                 <div class="relative text-gray-200 focus-within:text-gray-400">
@@ -31,7 +48,7 @@
                             <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd"></path>
                         </svg>
                     </div>
-                    <input id="search" class="block w-full rounded-md border border-transparent bg-black/25 py-2 pl-8 pr-3 leading-5 text-gray-400 placeholder-gray-500 focus:border-white focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1 focus:ring-offset-gray-600 text-sm" placeholder="Search" type="search" name="search">
+                    <input id="search" class="block w-full rounded-md border border-transparent bg-black/25 py-2 pl-8 pr-3 leading-5 text-gray-200 placeholder-gray-300 focus:border-white focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1 focus:ring-offset-gray-600 text-sm" placeholder="Search" type="search" name="search">
                 </div>
             </div>
         </div>
@@ -53,38 +70,38 @@
                             </div>
                             <div class="mt-5 h-0 flex-1 overflow-y-auto">
                                 <nav class="space-y-1 px-2">
-                                    <a href="#" class="bg-red-500/75 text-white group flex items-center px-2 py-2 text-base font-light rounded-md" x-state:on="Current" x-state:off="Default" x-state-description="Current: &quot;bg-gray-900 text-white&quot;, Default: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
+                                    <a href="/trending" class="bg-red-500/75 text-white group flex items-center px-2 py-2 text-base font-light rounded-md" x-state:on="Current" x-state:off="Default" x-state-description="Current: &quot;bg-gray-900 text-white&quot;, Default: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
                                         <svg class="text-gray-300 mr-4 flex-shrink-0 h-6 w-6" x-state:on="Current" x-state:off="Default" x-state-description="Current: &quot;text-gray-300&quot;, Default: &quot;text-gray-400 group-hover:text-gray-300&quot;" x-description="Heroicon name: outline/home" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
                                         </svg>
                                         TRENDING
                                     </a>
-                                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-base font-light rounded-md" x-state-description="undefined: &quot;bg-gray-900 text-white&quot;, undefined: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
+                                    <a href="/newest" class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-base font-light rounded-md" x-state-description="undefined: &quot;bg-gray-900 text-white&quot;, undefined: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
                                         <svg class="text-gray-400 group-hover:text-gray-300 mr-4 flex-shrink-0 h-6 w-6" x-state-description="undefined: &quot;text-gray-300&quot;, undefined: &quot;text-gray-400 group-hover:text-gray-300&quot;" x-description="Heroicon name: outline/users" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
                                         </svg>
                                         TERBARU
                                     </a>
-                                    <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 pb-4 text-base font-light rounded-md" x-state-description="undefined: &quot;bg-gray-900 text-white&quot;, undefined: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
+                                    <a href="/popular" class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 pb-4 text-base font-light rounded-md" x-state-description="undefined: &quot;bg-gray-900 text-white&quot;, undefined: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
                                         <svg class="text-gray-400 group-hover:text-gray-300 mr-4 flex-shrink-0 h-6 w-6" x-state-description="undefined: &quot;text-gray-300&quot;, undefined: &quot;text-gray-400 group-hover:text-gray-300&quot;" x-description="Heroicon name: outline/folder" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
                                         </svg>
-                                        POPULER
+                                        POPULAR
                                     </a>
-                                    @for ($i = 1; $i < 4; $i++)<a href="#" class="text-gray-400 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-1 text-small font-light rounded-md" x-state-description="undefined: &quot;bg-gray-900 text-white&quot;, undefined: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
+                                    @for ($i = 1; $i < 4; $i++)<a href="/cantik" class="text-gray-400 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-1 text-small font-light rounded-md" x-state-description="undefined: &quot;bg-gray-900 text-white&quot;, undefined: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
                                         <svg class="text-gray-400 group-hover:text-gray-300 mr-2 flex-shrink-0 h-4 w-4" x-state-description="undefined: &quot;text-gray-300&quot;, undefined: &quot;text-gray-400 group-hover:text-gray-300&quot;" x-description="Heroicon name: outline/calendar" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5" />
                                         </svg>
                                         cantik
                                     </a>
-                                    <a href="#" class="text-gray-400 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-1 text-base font-light rounded-md" x-state-description="undefined: &quot;bg-gray-900 text-white&quot;, undefined: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
+                                    <a href="/nsfw" class="text-gray-400 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-1 text-base font-light rounded-md" x-state-description="undefined: &quot;bg-gray-900 text-white&quot;, undefined: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
                                         <svg class="text-gray-400 group-hover:text-gray-300 mr-2 flex-shrink-0 h-4 w-4" x-state-description="undefined: &quot;text-gray-300&quot;, undefined: &quot;text-gray-400 group-hover:text-gray-300&quot;" x-description="Heroicon name: outline/calendar" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5" />
                                         </svg>
                                         nsfw
                                     </a>
-                                    <a href="#" class="text-gray-400 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-1 text-base font-light rounded-md" x-state-description="undefined: &quot;bg-gray-900 text-white&quot;, undefined: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
+                                    <a href="/otomotif" class="text-gray-400 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-1 text-base font-light rounded-md" x-state-description="undefined: &quot;bg-gray-900 text-white&quot;, undefined: &quot;text-gray-300 hover:bg-gray-700 hover:text-white&quot;">
                                         <svg class="text-gray-400 group-hover:text-gray-300 mr-2 flex-shrink-0 h-4 w-4" x-state-description="undefined: &quot;text-gray-300&quot;, undefined: &quot;text-gray-400 group-hover:text-gray-300&quot;" x-description="Heroicon name: outline/calendar" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5" />
                                         </svg>
@@ -99,7 +116,7 @@
                     </div>
                 </div>
             
-                <button type="button" class="inline-flex items-center justify-center rounded-md bg-black/25 text-gray-200 mt-2" aria-controls="mobile-menu" @click="open = !open" aria-expanded="false" x-bind:aria-expanded="open.toString()">
+                <button type="button" class="inline-flex items-center justify-center rounded-md bg-black/25 text-gray-200 mt-2 ml-2" aria-controls="mobile-menu" @click="open = !open" aria-expanded="false" x-bind:aria-expanded="open.toString()">
                     <span class="sr-only">Open main menu</span>
                     <svg class="h-6 w-6" x-description="Heroicon name: outline/bars-3-bottom-left" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"></path>
@@ -110,26 +127,31 @@
     </header>
 
     <main class="max-h-screen overflow-y-scroll snap snap-y snap-mandatory">
-        @for ($i = 1; $i < 10; $i++)<section class="relative w-full h-screen grid place-items-center text-gray-800 dark:text-gray-300 bg-white dark:bg-black snap-start">
-            <a href="/{{ $i }}.html">
-                <img src="/images/00{{ $i }}.jpg" width="640" height="960" alt="00{{ $i }}" title="00{{ $i }}" @if ($i > 1) loading="lazy"
-                    
-                @endif />
+        <h1 class="sr-only">Dear Boss! Siap laksanakan perintah.</h1>
+        @for ($i = 1; $i <= 24; $i++)<section class="relative w-full h-screen grid place-items-center text-gray-800 dark:text-gray-300 bg-white dark:bg-black snap-start">
+            <a href="/p/{{ $i }}">
+                <img src="/images/{{ $i }}.webp" width="640" height="960" alt="{{ $i }}" title="{{ $i }}"
+                    @if ($i > 1) loading="lazy"
+                    @else loading="eager"
+                    @endif />
             </a>
             <div class="absolute bottom-1/3 right-0 z-40 mr-2">
             </div>
             <blockquote class="absolute bottom-0 left-0 bg-gradient-to-b from-transparent to-black p-4 md:flex md:flex-grow md:flex-col">
 
                 <div class="text-base font-medium text-indigo-200 mb-2">
-                    <span class="inline-flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800">#cantik</span>
-                    <span class="inline-flex items-center rounded-full bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-800">#gadis</span>
+                    <a href="/cantik"><span class="inline-flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-base text-gray-800">#cantik</span></a>
+                    <a href="/fotogenik"><span class="inline-flex items-center rounded-full bg-red-100 px-2.5 py-0.5 text-xs font-base text-red-800">#fotogenik</span></a>
                 </div>
                 
                 <p class="relative text-sm font-light text-gray-300 line-clamp-2 md:flex-grow">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit unde ut esse cupiditate temporibus. Et aspernatur quis nemo impedit. Repellendus numquam quos aperiam laboriosam deleniti totam nemo alias ducimus maiores?</p>
                 <figure class="relative mt-2">
                     <div class="flex items-start">
                         <div class="inline-flex flex-shrink-0 rounded-full border-2 border-gray-300">
-                            <img class="h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                            <img class="h-12 w-12 rounded-full" src="/images/photo.avif" width="48" height="48" alt="Judith Black" title="Judith Black"
+                                @if ($i > 1) loading="lazy"
+                                @else loading="eager"
+                                @endif >
                         </div>
                         <div class="ml-4">
                             <div class="text-base font-medium text-white">Judith Black</div>
@@ -166,8 +188,6 @@
         </section>
         @endfor
     </main>
-
-    
 
 </body>
 
