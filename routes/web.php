3ef65bc70b860f07509943f1d95c8307af2a57c1
Reyno Andriano <reyno@gmail.com>
2023-01-05 06:49:02 +0700

    Change post display

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $faker = Faker\Factory::create();
-
     $posts = [];
     for ($i = 1; $i <= 24; $i++) {
         $posts[] = [
             'id' => $i,
             'image' => '/images/' . $i . '.webp',
-            'text' => $faker->paragraph(),
+            'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+officia deserunt mollit anim id est laborum.',
             'loading' => ($i <= 5) ? 'eager' : 'lazy'
         ];
     }
